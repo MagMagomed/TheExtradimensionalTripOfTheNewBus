89f8d739ac2f6eda060b75e48a7474e9c114118e
LAPTOP-F0NQ82JO\mahen <mahendropolos@yandex.ru>
2023-04-25 21:57:09 +0300

    Добавил тег и логику восстановления игровка в вертикальное состояние

--- My project/Assets/Player/Scripts/PlayerController.cs
+++ My project/Assets/Player/Scripts/PlayerController.cs
@@ -12,11 +12,38 @@ public class PlayerController : MonoBehaviour
     {
         rigidbody = GetComponent<Rigidbody2D>();
     }
+    private void FixedUpdate()
+    {
 
+    }
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.rotation.z);
+        if (Mathf.Abs(transform.rotation.z) >= 0.5f && Mathf.Abs(GetComponent<Rigidbody2D>().angularVelocity) < 0.3f )
+        {
+
+            float torque = -45f;
+            rigidbody.AddTorque(torque);
+        }
+        if (Mathf.Abs(transform.rotation.z) <= -0.5f && Mathf.Abs(GetComponent<Rigidbody2D>().angularVelocity) > -0.3f )
+        {
+
+            float torque = 45f;
+            rigidbody.AddTorque(torque);
+        }
         camera.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
-        rigidbody.AddForce(Vector2.right * acceleration);
+        //rigidbody.AddForce(Vector2.right * acceleration);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y < -0.5f && collision.gameObject.tag == "Ground") // Проверяем, что объект перевернулся
+        {
+            float rotationZ = transform.eulerAngles.z;
+            if (rotationZ > 180) rotationZ -= 360;
+            float torque = -rotationZ * 10f; // Добавляем крутящий момент, чтобы объект повернулся в правильное положение
+            rigidbody.AddTorque(torque);
+        }
     }
 }
